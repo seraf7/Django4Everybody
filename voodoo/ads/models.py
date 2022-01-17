95262059239e6b9a541e52243032e9cbfe666173
seraf7 <zaske_4@hotmail.com>
2022-01-17 15:38:25 -0600

    Adición de campo para carga de imágenes

--- voodoo/ads/models.py
+++ voodoo/ads/models.py
@@ -16,6 +16,11 @@ class Ad(models.Model):
     # Guarda atomáticamente fecha/hora de la ultima modificación
     updated_at = models.DateTimeField(auto_now=True)
 
+    # Campos para guardar imagen
+    picture = models.BinaryField(null=True, editable=True)
+    content_type = models.CharField(max_length=256, null=True,
+        help_text='The MIMEType of the file')
+
     # Método para imprimir objeto como cadena
     def __str__(self):
         return self.title
