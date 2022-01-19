300f2097e80baaf495400f5ddae2b9adb52f7142
seraf7 <zaske_4@hotmail.com>
2022-01-19 16:31:03 -0600

    Registro de comentarios en sitio de administración

--- voodoo/ads/admin.py
+++ voodoo/ads/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from ads.models import Ad
+from ads.models import Ad, Comment
 
 # Añade los modelos a la página de administración
 admin.site.register(Ad)
+admin.site.register(Comment)
